ad3b0c67c005a7476bf73558d0ce33a86e053986
sun <sleepsun123@163.com>
2021-04-05 22:40:41 +0800

    add clash.sh file

---
+++ clash.sh
@@ -0,0 +1,44 @@
+# 进程名
+process="clash"
+
+# 获取进程ID
+PID=$(ps -C "$process" --no-header |wc -l)
+
+case "$1" in
+start)
+  if [ "$PID" -eq 0 ]; then
+    nohup /etc/clash/clash-linux-amd64 -d /etc/clash > /dev/null 2>&1 &
+    echo "The "$process" is start..."
+  else
+    echo "The "$process" is running..."
+  fi
+  ;;
+restart)
+  if [ "$PID" -eq 0 ]; then
+    echo "The "$process" not running..."
+  else
+    kill -9 $PID
+    nohup /etc/clash/clash-linux-amd64 -d /etc/clash > /dev/null 2>&1 &
+    echo "The "$process" is restart..."
+  fi
+  ;;
+stop)
+  if [ "$PID" -eq 0 ]; then
+    echo "The not running..."
+  else
+    kill -9 "$PID"
+    echo "The "$process" is stop..."
+  fi
+  ;;
+status)
+  if [ "$PID" -eq 0 ]; then
+    echo "The "$process" not running..."
+  else
+    echo "The "$process" is running"
+  fi
+  ;;
+*)
+  echo "Usage:{start|restart|stop|status}"
+  ;;
+
+esac
--- entrypoint.sh
+++ entrypoint.sh
@@ -50,16 +50,6 @@ set_clash_iptables
 
 #echo "1" > /proc/sys/net/ipv4/ip_forward
 
-if [ ! -e '/clash_config/config.yaml' ]; then
-    echo "init /clash_config/config.yaml"
-    cp  /root/.config/clash/config.yaml /clash_config/config.yaml
-fi
-
-if [ ! -e '/clash_config/Country.mmdb' ]; then
-    echo "init /clash_config/Country.mmdb"
-    cp  /root/.config/clash/Country.mmdb /clash_config/Country.mmdb
-fi
-
-ip addr
+/bin/sh /etc/clash/clash.sh start
 
 exec "$@"
