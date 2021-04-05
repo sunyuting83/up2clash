b7aa8b60d399da580768df3310569bfb13c8dfff
sun <sleepsun123@163.com>
2021-04-05 12:05:39 +0800

    suburl

--- app.go
+++ app.go
@@ -21,7 +21,7 @@ func main() {
 	app := gin.New()
 	app.GET("/", func(c *gin.Context) {
 		var geturl string = c.DefaultQuery("geturl", "https://pub-api-1.bianyuan.xyz")
-		var suburl string = c.DefaultQuery("suburl", "https://rss.srss.xyz/link/IBbFyr6dRelkPM0a?mu=2")
+		var suburl string = c.DefaultQuery("suburl", "https://jiang.netlify.app")
 		suburl = url.QueryEscape(suburl)
 		var exclude string = c.DefaultQuery("exclude", "%e7%be%8e%e5%9b%bd%7c%e7%88%b1%e6%b2%99%e5%b0%bc%e4%ba%9a%7c%e7%bd%97%e9%a9%ac%e5%b0%bc%e4%ba%9a%7c%e5%8a%a0%e6%8b%bf%e5%a4%a7%7c%e5%8c%97%e7%be%8e%7c%e4%bf%84%e7%bd%97%e6%96%af%7c%e5%85%8b%e7%bd%97%e5%9c%b0%e4%ba%9a%7c%e4%b9%8c%e5%85%8b%e5%85%b0%7c%e6%ac%a7%e7%9b%9f%7c%e6%91%a9%e5%b0%94%e5%a4%9a%e7%93%a6%7cBuLink%7c%e5%9f%83%e5%8f%8a%7c%e8%91%a1%e8%90%84%e7%89%99%7c%e6%8b%89%e8%84%b1%e7%bb%b4%e4%ba%9a%7c%e8%8b%b1%e5%9b%bd%7c%e8%a5%bf%e7%8f%ad%e7%89%99%7c%e7%91%9e%e5%85%b8%7c%e5%8d%a2%e6%a3%ae%e5%a0%a1%7c%e5%be%b7%e5%9b%bd%7c%e7%88%b1%e5%b0%94%e5%85%b0%7c%e6%b3%a2%e5%85%b0%7c%e6%8d%b7%e5%85%8b%7c%e6%84%8f%e5%a4%a7%e5%88%a9%7cisx.yt%7c%e8%8d%b7%e5%85%b0%7cNL%7cUS%7cDE%7cFR%7cAU%7cRU%7cRO%7cZZ%7cGB%7c%e5%8d%b0%e5%ba%a6%7c%e4%b8%b9%e9%ba%a6")
 		var ConfigPath string = c.DefaultQuery("config", "/etc/clash/config.yaml")
